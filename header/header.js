a345cbd6971ccb4f3a57760402c5e621451815e2
green <avk4935@gmail.com>
2025-11-30 17:16:09 +0700

    11301716

--- header/header.js
+++ header/header.js
@@ -8,7 +8,9 @@ class Header {
 
     this.img = document.createElement("img");
     this.img.className = "header__img";
-    this.img.src = "images/eisenhower-matrix.jpg";
+	const basePath = new URL('.', import.meta.url); 
+	this.img.src = new URL('../images/eisenhower-matrix.jpg', basePath).href;
+    // this.img.src = "images/eisenhower-matrix.jpg";
     this.img.alt = "Eisenhower Matrix Diagram"
 
     this.link = document.createElement("a");
